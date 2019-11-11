3f1f32f0eab169188681e873471442c2f2d26e97
cdmccauley <cdmccauley@gmail.com>
2019-11-10 17:40:50 -0700

    started script file code

--- course-timeline.js
+++ course-timeline.js
@@ -1,6 +1,19 @@
-// debug
+// confirm script file is loaded
 console.log('course-timeline.js loaded');
 
-/* 
- * 
- */
+// declarations
+let numModulesInput;
+let submitBtn;
+
+// elements
+numModulesInput = document.getElementById('num-modules');
+submitBtn = document.getElementById('submit');
+
+// event listeners
+numModulesInput.addEventListener('change', function() {
+    console.log('numModulesInput change listener');
+});
+
+submitBtn.addEventListener('click', function(){
+    console.log('submitBtn click listener');
+});
